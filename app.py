a756e3a6eed49ff3c9029cafca59880a0f3f0c28
Bryx28 <qjbfquebral@tip.edu.ph>
2022-03-05 18:58:03 +0800

    Revision 2

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class User(db.Model,UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('post', backref='author', lazy=True)
 
     def  __repr__(self):
         return f"User({self.username} - {self.user_fname} {self.user_lname})"
