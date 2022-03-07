abc76e4453124591df789d6a4984e2792b664011
Bryx28 <qjbfquebral@tip.edu.ph>
2022-03-08 02:09:00 +0800

    Post Functionality

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ class User(db.Model,UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    author = db.relationship("public.'post'", backref="author", lazy=True)
 
     def  __repr__(self):
         return f"User({self.username} - {self.user_fname} {self.user_lname})"
@@ -46,8 +47,7 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer,  nullable=False)
-    #db.ForeignKey('user.user_id')
+    user_id = db.Column(db.Integer, db.ForeignKey("public.user.user_id"), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
