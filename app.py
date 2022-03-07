4dd4d8429b5014420f95f9fb7ecf6ec4070d67a5
Bryx28 <qjbfquebral@tip.edu.ph>
2022-03-08 02:30:41 +0800

    Post Functionality

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class User(db.Model,UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    author = db.relationship("public.'post'", backref="author", lazy=True)
+    author = db.relationship("Post", backref="author", lazy=True)
 
     def  __repr__(self):
         return f"User({self.username} - {self.user_fname} {self.user_lname})"
