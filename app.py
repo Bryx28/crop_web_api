da5cab5618689cae2e620b3e3976320ccd796cf4
Bryx28 <qjbfquebral@tip.edu.ph>
2022-03-08 02:12:45 +0800

    Post Functionality

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("public.user.user_id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("public.'user'.user_id"), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
